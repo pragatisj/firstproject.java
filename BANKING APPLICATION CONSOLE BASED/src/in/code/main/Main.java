274f8a4b6b0f574f59294c3ac34db2e654fda3ba
pragatsj <pragatijadhav2121@gmail.com>
2023-04-10 12:34:33 +0530

    thirdcommit

---
+++ BANKING APPLICATION CONSOLE BASED/src/in/code/main/Main.java
@@ -0,0 +1,13 @@
+package in.code.main;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Operation operlation = new Operation();
+		operlation.bankinfo();
+	
+	
+	}
+
+}
